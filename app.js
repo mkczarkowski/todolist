c5a6263d7ccda79fb5f64f1379f74254e4115302
marczarkowski <marcinczarkowski@onet.eu>
2017-01-31 11:13:49 +0100

    Documentation updated

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var bodyParser = require('body-parser');
 var app = express();
 var DocumentDBClient = require('documentdb').DocumentClient;
 var config = require('./config');
-var TaskList = require('./routes/taskController');
+var TaskController = require('./routes/taskController');
 var TaskModel = require('./models/taskModel');
 
-// ustawienie silnika Jade odpowiadającego za wyświetlanie struktury projektu
+// ustawienie ścieżki do silnika Jade odpowiadającego za wyświetlanie struktury projektu
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -25,12 +25,12 @@ var docDbClient = new DocumentDBClient(config.host, {
     masterKey: config.authKey
 });
 var taskModel = new TaskModel(docDbClient, config.databaseId, config.collectionId);
-var taskList = new TaskList(taskModel);
+var taskController = new TaskController(taskModel);
 taskModel.init();
 
-app.get('/', taskList.displayTasks.bind(taskList));
-app.post('/addtask', taskList.addTask.bind(taskList));
-app.post('/completetask', taskList.completeTask.bind(taskList));
+app.get('/', taskController.displayTasks.bind(taskController));
+app.post('/addtask', taskController.addTask.bind(taskController));
+app.post('/completetask', taskController.completeTask.bind(taskController));
 app.set('view engine', 'jade');
 
 // skrypt łapiący błąd '404 Not Found' i przekazujący go do obsługi błędów
